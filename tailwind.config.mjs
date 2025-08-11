d93add952a7617935e5698db2f8bb10a043cb974
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2025-08-10 21:34:29 -0400

    add: hero section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,15 +4,19 @@ export default {
     extend: {
       colors: {
         brand: {
-          light: '#f5f5f5',
-          DEFAULT: '#512173',
-          dark: '#231037',
-          darker: '#130a22',
-        }
+          light: "#f5f5f5",
+          DEFAULT: "#512173",
+          dark: "#231037",
+          darker: "#130a22",
+        },
+        accent: {
+          DEFAULT: "#E32887",
+          darker: "#CA2075",
+        },
       },
       backgroundImage: {
-        'brand-gradient': 'linear-gradient(90deg, #130a22, #231037, #512173)',
-      }
+        "brand-gradient": "linear-gradient(90deg, #130a22, #231037, #512173)",
+      },
     },
   },
   plugins: [],
