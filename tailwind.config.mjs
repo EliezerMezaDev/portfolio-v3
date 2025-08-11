5a06bf47b54e588d60b57c4070470ad77f0cc802
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2025-08-10 20:56:04 -0400

    add: tailwind

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          light: '#f5f5f5',
+          DEFAULT: '#512173',
+          dark: '#231037',
+          darker: '#130a22',
+        }
+      },
+      backgroundImage: {
+        'brand-gradient': 'linear-gradient(90deg, #130a22, #231037, #512173)',
+      }
+    },
+  },
+  plugins: [],
+};
