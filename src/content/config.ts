2d5361ffbab6f70b998d913e8ef73415070937e4
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2025-08-16 13:51:04 -0400

    add: prettier

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,34 @@ const experienceCollection = defineCollection({
   }),
 });
 
+const projectsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    summary: z.string(),
+    tags: z.array(z.string()),
+    isFeatured: z.boolean().default(false),
+
+    portraitImage: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+
+    galleryImages: z
+      .array(
+        z.object({
+          src: z.string(),
+          alt: z.string(),
+        })
+      )
+      .optional(),
+
+    demoUrl: z.string().url().optional(),
+    repoUrl: z.string().url().optional(),
+  }),
+});
+
 export const collections = {
   experience: experienceCollection,
+  projects: projectsCollection,
 };
